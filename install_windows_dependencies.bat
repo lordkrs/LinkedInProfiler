f1e07bc3f3448285bdc4d4c60232dde2e06cc383
karthikshastry <karthik@workflowlabs.com>
2020-11-25 16:13:23 +0530

    installer added

---
+++ install_windows_dependencies.bat
@@ -0,0 +1,3 @@
+%~dp0python-3.9.0-amd64.exe InstallAllUsers=1 TargetDir=C:\Python3
+C:\Python3\python.exe -m pip install xmltodict xlsxwriter openpyxl bottle googlesearch
+pause
---
+++ start_server.bat
@@ -0,0 +1,3 @@
+SET PROFILER_PATH=%~dp0profiler.py
+C:\Python27\python.exe %PROFILER_PATH%
+pause
